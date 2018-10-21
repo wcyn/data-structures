2dc634362e82f902decc6e0bddd891ce758a0012
Cynthia Abura <wasonga.cynthia@gmail.com>
2018-10-21 13:11:01 +0300

    Implement Linear time fibonacci and linear sum

--- Python/essentials/recursion.py
+++ Python/essentials/recursion.py
@@ -46,3 +46,31 @@ def r_binary_search(target, num_list, low, high):
 
 print("Recursive binary search", r_binary_search(23, nums, 0, len(nums) - 1))
 
+def bad_fibonacci(num):
+    """O(2^n)"""
+    if num <= 1:
+        return num
+    else:
+        return bad_fibonacci(num - 2) + bad_fibonacci(num - 1)
+
+print("Bad Fibonacci: ", bad_fibonacci(4))
+
+def good_fibonacci(num):
+    """O(n)"""
+    if num <= 1:
+        return (num, 0)
+    else:
+        (a, b) = good_fibonacci(num-1)
+        print("a:{} , b:{}".format(a, b))
+        return (a+b, a)
+print("Good Fibonacci: ", good_fibonacci(4))
+
+
+def linear_sum(num_list, n):
+    if n == 0:
+        return 0
+    else:
+        return linear_sum(num_list, n-1) + num_list[n-1]
+
+nums = [1,2,3,4,5]
+print("Linear Sum: ", linear_sum(nums, 5))
