8a5e7f756cf684a9ab940a48df28149d3e87d8f1
Cynthia Wasonga <wasonga.cynthia@gmail.com>
2019-01-25 12:07:23 +0300

    Implement a function to legally color a graph

---
+++ Python/Cake/graph_coloring.py
@@ -0,0 +1,48 @@
+# Given an undirected graph ↴ with maximum degree ↴ D,
+# find a graph coloring ↴ using at most D+1 colors.
+
+
+class GraphNode:
+    def __init__(self, label, neighbours=set()):
+        self.label = label
+        self.neighbours = neighbours
+        self.color = None
+
+
+a = GraphNode('a')
+b = GraphNode('b', {a})
+c = GraphNode('c', {b, a})
+
+a.neighbours.add(b)
+a.neighbours.add(b)
+a.neighbours.add(c)
+b.neighbours.add(c)
+
+graph = [a, b, c]
+
+
+def get_max_degree(graph):
+    max_degree = 0
+    for node in graph:
+        max_degree = max(max_degree, len(node.neighbours))
+    return max_degree
+
+
+print(get_max_degree(graph))
+
+
+def color_graph(graph):
+    colors = [num for num in range(get_max_degree(graph) + 1)]
+    for node in graph:
+        for color in colors:
+            if color not in {n.color for n in node.neighbours}:
+                node.color = color
+                break
+
+
+print([(n.label, n.color) for n in graph])
+color_graph(graph)
+print([(n.label, n.color) for n in graph])
+
+
+
